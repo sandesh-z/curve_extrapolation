5d5c2338e7212ae2bc67b8202b615b7c9edb98b8
sandesh <gsandace@gmail.com>
2024-03-21 13:08:09 +0545

    instruction widget added in homepage

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:curve_extrapolation/home_bloc/home_bloc.dart';
 import 'package:curve_extrapolation/screens/draggable_screen.dart';
 import 'package:curve_extrapolation/utils/spline_painter.dart';
+import 'package:curve_extrapolation/widgets/instructions.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -15,9 +16,10 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   double posx = 100.0;
   double posy = 100.0;
-
+  bool showIntrsuctions = true;
   List<Offset> offsets = [];
 
+  ///update and add offset after each points are drawn
   void onTapDown(BuildContext context, TapDownDetails details) {
     late Offset localOffset;
     final RenderObject? box = context.findRenderObject();
@@ -39,23 +41,25 @@ class _HomeScreenState extends State<HomeScreen> {
       backgroundColor: const Color(0xff5D2EC0),
       floatingActionButton: FloatingActionButton(
           onPressed: () {
-            context.read<HomeBloc>().add(HomeEvent.addControlPoints(offsets));
-            Navigator.of(context).push(MaterialPageRoute(
-                builder: (_) => DraggableScreen(offsets: offsets)));
+            navigateToNextScreen(context: context, length: offsets.length);
           },
           child: const Text("Next")),
       body: Stack(
         children: [
+          showIntrsuctions
+              ? InstructionWidget(
+                  onTap: () {
+                    setState(() {
+                      showIntrsuctions = false;
+                    });
+                  },
+                )
+              : const SizedBox(),
           GestureDetector(
             onTapDown: (details) => onTapDown(context, details),
-            child: Center(
-              child: Container(
-                color: Colors.red,
-                height: 400,
-                width: double.infinity,
-              ),
-            ),
+            child: backgroundContainer(context: context),
           ),
+          //draw each points
           for (final offset in offsets)
             Positioned(
               left: offset.dx - 10,
@@ -67,11 +71,39 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           offsets.length > 4
               ? CustomPaint(
-                  painter: SplinePainter(offset: offsets, strokeWidth: 2.0),
+                  painter:
+                      SplinePainter(offset: offsets, strokeWidth: 2.0, context),
                 )
               : Container()
         ],
       ),
     );
+  }
+
+  void showSnackBar({required String message}) {
+    var snackBar = SnackBar(
+      content: Text(message),
+      backgroundColor: Colors.purple.withOpacity(.7),
+      elevation: 10,
+      behavior: SnackBarBehavior.floating,
+      margin: const EdgeInsets.all(5),
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  void navigateToNextScreen(
+      {required BuildContext context, required int length}) {
+    if (offsets.length < 5) {
+      showSnackBar(message: "Please enter all 5 points");
+      return;
+    }
+    context.read<HomeBloc>().add(HomeEvent.addControlPoints(offsets));
+    //navigate
+
+    Navigator.of(context).pushAndRemoveUntil(
+        MaterialPageRoute(
+          builder: (_) => DraggableScreen(offsets: offsets),
+        ),
+        (route) => false);
   }
 }
